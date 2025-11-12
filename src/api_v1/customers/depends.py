c8108dec5d118a35e5875f6c043f9ba1f64fdcd6
RomesAll <romesky845@gmail.com>
2025-11-12 15:47:15 +0300

    Добавил crud операции для взаимодействия с бд

--- src/api_v1/customers/depends.py
+++ src/api_v1/customers/depends.py
@@ -7,8 +7,7 @@ async def get_customers_by_id(customer_id: int):
     result_query = await CustomersDAO().get_customers_by_id_dao(customer_id)
     if result_query:
         return result_query
-    raise HTTPException(status_code=404, detail='Customer not found')
-    
+    raise HTTPException(status_code=404, detail='Customer not found')    
 
 CustomersIdDep = Annotated[int, Depends(get_customers_by_id)]
 CustomersInfoDep = Annotated[CustomerPATCHSchemas, Depends(CustomerPATCHSchemas)]
