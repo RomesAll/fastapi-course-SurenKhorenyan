a6c4fcf6729f5ba129b9b74df13e29fe28edd7ce
RomesAll <romesky845@gmail.com>
2025-11-19 13:52:25 +0300

    Рализация Token basic auth

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi import FastAPI, Depends, HTTPException, status, Header, Request
 import uvicorn, secrets
 from typing import Annotated
 from fastapi.security import HTTPBasicCredentials, HTTPBasic
 
 app = FastAPI()
+''' Basic auth
 security = HTTPBasic()
 user_db = {
     'admin': '123',
     'roman': 'qwerty',
 }
-
 def get_user_info(credentials: Annotated[HTTPBasicCredentials, Depends(security)]):
     exception_401 = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='User not found')
     user_password = user_db.get(credentials.username, None)
@@ -25,7 +25,25 @@ def demo_auth_credentials(credentials: Annotated[HTTPBasicCredentials, Depends(s
 
 @app.get('/auth/userinfo')
 def demo_auth_get_user_info(username: str = Depends(get_user_info)):
-    return {'message': f'Hello {username}'}
+    return {'message': f'Hello {username}'}'''
+
+''' Token basic auth
+user_info = {
+    '66d5f2222990e2a42a796277372ccbb12284aa9af5dc9233101e6818187d37d0': 'amdin',
+    '064dafd23cac1b9fd7db239b1a83657b13410938c0d1bcdd7f552b3089041bce': 'roman'
+}
+
+def get_user_info(request: Request, token = Header(alias='x-auth-token')):
+    username = user_info.get(token, None)
+    if username is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Invalid token')
+    return username
+
+@app.get('/auth/token')
+def get_username(username = Depends(get_user_info)):
+    return {"message": f"Hello - {username}"}
+'''
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
